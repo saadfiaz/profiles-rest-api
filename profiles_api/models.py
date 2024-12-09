d16cdf9184fd0e9000d8c28e401c12fac8d06036
saadfiaz2 <saad.fiaz@argonteq.com>
2024-12-09 12:18:05 +0500

    added Hello Api View

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,55 +2,56 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
-# Create your models here.
 class UserProfileManager(BaseUserManager):
-    """Manager for userprofiles"""
+    """Manager for user profiles"""
 
-    def Create_user(self, email, name, password=None):
-        """Create a new profile"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User Must have an email address')
+            raise ValueError('User must have an email address')
 
         email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_superuser(self, email, name, password):
-        """Create and save a new supreruser with the give details"""
-        user = self._create_user(email, name, password)
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
+
         return user
 
 
-
-
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database Model for users in the System"""
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=225)
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDs = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of the user"""
+        """Retrieve full name of user"""
         return self.name
 
     def get_short_name(self):
-        """Retrive Short Name of the user"""
+        """Retrieve shot name of user"""
         return self.name
 
     def __str__(self):
-        """return String Representation of the user"""
+        """Return string representation of our user"""
         return self.email
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPiView(APIView):
+    """Test API View"""
+    def get(self, request, format=None):
+        """Returns a list of Api View features"""
+        an_apiview = [
+            "perfect for implementing complex logics",
+            "calling other apis",
+            "working with local files",
+            "Is mapped manually to the urls"
+        ]
+        return Response({"message":"hello", "an_apiview": an_apiview})
